a6180b602c81ee58fa64eb76eec659ec4b7b0db6
Random-user008 <randommail008008@gmail.com>
2022-01-04 19:17:04 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import LandingMain from './Components/Landing/landingMain';
+import Services from './Components/Landing/Services';
+import AppLink from './Components/Landing/AppLink';
+import Footer from './Components/Landing/Footer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <LandingMain />
+        <Services />
+        <AppLink />
+        <Footer />     
     </div>
   );
 }
